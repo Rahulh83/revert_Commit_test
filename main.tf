d559794ace2b8c30ddb63cbd2ab765ef6c87266e
unknown <rahulharsh261@outlook.com>
2025-12-10 02:57:43 +0530

    stg added

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 resource "azurerm_resource_group" "RG" {
     name     = "revert-commit-test-rg"
     location = "eastus"
+    managed_by = "user"
 
+}
+resource "azurerm_storage_account" "SA" {
+    name                     = "revertcommittestsa"
+    resource_group_name      = azurerm_resource_group.RG.name
+    location                 = azurerm_resource_group.RG.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
 }
